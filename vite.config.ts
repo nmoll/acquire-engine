94c4e351caee8c9f5994f98f5e0ec062b88d47f6
Nathan Moll <nathan.moll93@gmail.com>
2023-06-16 18:15:07 -0600

    replace firebase with gundb

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
+// required for gun to work
+const moduleExclude = (match) => {
+  const m = (id) => id.indexOf(match) > -1;
+  return {
+    name: `exclude-${match}`,
+    resolveId(id) {
+      if (m(id)) return id;
+    },
+    load(id) {
+      if (m(id)) return `export default {}`;
+    },
+  };
+};
+
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: [
+      "gun",
+      "gun/gun",
+      "gun/sea",
+      "gun/sea.js",
+      "gun/lib/then",
+      "gun/lib/webrtc",
+      "gun/lib/radix",
+      "gun/lib/radisk",
+      "gun/lib/store",
+      "gun/lib/rindexed",
+    ],
+  },
+  plugins: [moduleExclude("text-encoding")],
   // plugins: [
   //   VitePWA({
   //     includeAssets: [
